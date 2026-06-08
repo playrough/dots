1aacec13e5237fb346a01f05dab36990c619951d
Playrough <106418248+playrough@users.noreply.github.com>
2026-06-08 22:44:49 +0700

    remove websocket and create colors.meta api

--- .config/web/server.js
+++ .config/web/server.js
@@ -1,39 +1,48 @@
 import express from "express";
 import { createServer } from "http";
-import { WebSocketServer } from "ws";
 import { watch } from "chokidar";
 import { readFileSync } from "fs";
 
 const app = express();
 const server = createServer(app);
-const wss = new WebSocketServer({ server });
-const WS_STATE = {
-  CONNECTING: 0,
-  OPEN: 1,
-  CLOSING: 2,
-  CLOSED: 3,
-};
 
 const CSS_PATH = "./colors.css";
 
-app.get("/colors.css", (_req, res) => {
-  res.setHeader("Content-Type", "text/css");
-  res.send(readFileSync(CSS_PATH, "utf8"));
-});
+let cachedCSS = "";
+let lastModified = Date.now();
 
-function broadcast(msg) {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WS_STATE.OPEN) {
-      client.send(msg);
-    }
-  });
+// load initial
+function loadCSS() {
+  cachedCSS = readFileSync(CSS_PATH, "utf8");
+  lastModified = Date.now();
 }
 
+loadCSS();
+
+// watch file
 watch(CSS_PATH).on("change", () => {
   console.log("[WATCH] CSS changed");
-  broadcast("changed");
+  loadCSS();
+});
+
+// simple endpoint
+app.get("/colors.css", (_req, res) => {
+  res.setHeader("Content-Type", "text/css");
+  res.setHeader("Cache-Control", "no-cache");
+
+  // optional: simple ETag-like behavior
+  res.setHeader("X-Theme-Version", String(lastModified));
+
+  res.send(cachedCSS);
+});
+
+// optional: lightweight "check update" endpoint (better than downloading full css every time)
+app.get("/colors.meta", (_req, res) => {
+  res.json({
+    version: lastModified,
+  });
 });
 
 server.listen(8765, "127.0.0.1", () => {
-  console.log("Server running");
+  console.log("Server running on http://127.0.0.1:8765");
 });
